f89ee177694db1ba8f5c08b3df4639c7a7ab79ba
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-20 22:19:24 +0300

    Add taxt to post

--- db/migrate/20220920153113_create_posts.rb
+++ db/migrate/20220920153113_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :text
       t.integer :like_counter
       t.integer :comment_counter
 
