4a3a1a77e11c8db36f50ae6250bb4e8071f8dbc5
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-20 21:33:55 +0300

    Fix linter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_920_165_645) do
+ActiveRecord::Schema[7.0].define(version: 20_220_920_165_645) do # rubocop:todo Metrics/BlockLength
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
