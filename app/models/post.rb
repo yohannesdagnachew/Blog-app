761de0eec7ef12b5581a40b0869fb2b3f9306e9b
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-22 23:45:56 +0300

    Fix linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  validates :title, presence: true, length: {maximum: 250}
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
