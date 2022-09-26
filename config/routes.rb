6e92ab7e9319eb9aff4e97ef6d651d2bec510b0c
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-26 21:17:48 +0300

    Make routing code short

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index', as: :users
-  get '/users/:id', to: 'users#show', as: :user
-  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
