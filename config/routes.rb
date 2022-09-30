a8c1b92fdb524a17855cdf7ba9c7be27c7127d6b
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-30 13:58:02 +0300

    Fix linter

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show]
   end
-  
+
   get '/users/:user_id/posts/:id/comments', to: 'comment#new_comment', as: :new_comment
   post '/users/:user_id/posts/:id/comments/new_comment', to: 'comment#create_comment', as: :create_comment
   get '/users/:user_id/posts/:id/like', to: 'like#like', as: :like
--- spec/requests/like_request_spec.rb
+++ spec/requests/like_request_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe 'Likes', type: :request do
+RSpec.describe 'Likes', type: :request do # rubocop:todo Lint/EmptyBlock
 end
