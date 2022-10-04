edd51df5dbdf7afcd8ab1bdf1fa694632afc1cc5
Favour Ezeugwa <favourezeugwa@gmail.com>
2022-10-04 13:22:32 +0100

    Add comments instances and save all

--- spec/features/posts_index_spec.rb
+++ spec/features/posts_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Post index page', type: :system do
-  describe 'GET /postss/index' do
+  describe 'GET /posts/index' do
     before(:each) do
       @user_1 = User.create(name:'John',photo: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80',bio:'John is a great person.')
 
@@ -9,5 +9,17 @@ RSpec.describe 'Post index page', type: :system do
       @post_2 = Post.create(title:'Post 2',text:'This is the second post.',author:@user_1)
       @post_3 = Post.create(title:'Post 3',text:'This is the third post.',author:@user_1)
       @post_4 = Post.create(title:'Post 4',text:'This is the fourth post.',author:@user_1)
+      
+      @comment_1 = Comment.create(text:'This is the first comment.',author:@user_1,post:@post_1)
+      @comment_2 = Comment.create(text:'This is the second comment.',author:@user_1,post:@post_1)
+      @comment_3 = Comment.create(text:'This is the third comment.',author:@user_1,post:@post_1)
+      
+      @post_1.save
+      @post_2.save
+      @user_1.save
+
+      @comment_1.save
+      @comment_2.save
+      @comment_3.save
     end
   end
