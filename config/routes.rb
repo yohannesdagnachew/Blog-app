3bdaab2f9aced495bfc2d35852d428754f07cd93
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-08 08:48:20 +0300

    Add routing for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index new create show] do # rubocop:todo Lint/EmptyBlock
-    end
+    namespace 'api' do
+      resources :posts
+    end  
+    resources :posts, only: %i[index new create show] do
+      namespace 'api' do
+        resources :comments
+      end
+    end  
   end
 
   root to: 'main#index'
   get '/users/:user_id/posts/:id/comments', to: 'comment#new_comment', as: :new_comment
   post '/users/:user_id/posts/:id/comments/new_comment', to: 'comment#create_comment', as: :create_comment
   get '/users/:user_id/posts/:id/like', to: 'like#like', as: :like
+
 end
+
